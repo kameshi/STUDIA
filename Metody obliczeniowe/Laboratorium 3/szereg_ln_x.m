bba7e83cb624ff913e7947fb30d849c1b473bdf1
Bartłomiej Osak <barosa1996@hotmail.com>
2017-10-23 14:01:44 +0200

    MO.LAB3.MC.PROSTOKAT_NAD.PROSTOKAT_NIEDOM.SIMPSON.TRAPEZ

---
+++ Metody obliczeniowe/Laboratorium 3/szereg_ln_x.m
@@ -0,0 +1,9 @@
+function[wynik] = szereg_ln_x(x,n)
+wynik = 0;
+    if(x>1) || (x<-1)
+        disp('Blad');
+    end
+    for i=1 : n
+        wynik = wynik + (power((-1),(i+1))/i)*power(x-1,i);
+    end;
+end
