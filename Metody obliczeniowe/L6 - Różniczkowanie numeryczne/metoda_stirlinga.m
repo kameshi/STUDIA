046ec38f40e208326a7b479eb9c84043156045d0
Bartłomiej Osak <barosa1996@hotmail.com>
2017-11-18 20:10:40 +0100

    MO.LAB6.TAYLOR.STIRLING

--- Metody obliczeniowe/L6 - Różniczkowanie numeryczne/metoda_stirlinga.m
+++ Metody obliczeniowe/L6 - Różniczkowanie numeryczne/metoda_stirlinga.m
@@ -1,4 +1,4 @@
-function[C] = metoda_stirlinga(X,Y,h)
+function[C] = metoda_stirlinga(X,Y,h,s)
     n = size(X,2);
     T=zeros([size(X,2) size(X,2)+1]);
     T(:,1) = X;
@@ -11,9 +11,18 @@ function[C] = metoda_stirlinga(X,Y,h)
     end
     disp(T);
     a=[1,-1/6,1/30];
-    for i=2:2:n
-        disp(i);
-        fx = fx + a(i/2)*(0.5*(T((((n-i)+1)/2),i+1)+T((((n-i)+1)/2+1),i+1)));
+    a2=[1,1/12,1/90];
+    if s==1
+        for i=2:2:n
+            fx = fx + a(i/2)*(0.5*(T((((n-i)+1)/2),i+1)+T((((n-i)+1)/2+1),i+1)));
+        end
+        C = fx * 1/h;
     end
-    C = fx * 1/h;
+    if s==2
+        fx=0;
+        for i=2:2:n
+            fx = fx + a2(i/2)*(T((((n-i)+1)/2),i+2));
+        end
+        C = fx * 1/(h^2);
+    end
 end
--- Metody obliczeniowe/L6 - Różniczkowanie numeryczne/metoda_taylora.m
+++ Metody obliczeniowe/L6 - Różniczkowanie numeryczne/metoda_taylora.m
@@ -1,4 +1,4 @@
-function[Fx] = metoda_taylora(X,Y,h)
+function[Fx] = metoda_taylora(X,Y,h,s)
     n = size(X,2);
     T = zeros(n,n+1);
     T(:,1) = X;
@@ -8,7 +8,27 @@ function[Fx] = metoda_taylora(X,Y,h)
         for j = n-(i-2):-1:1
             T(j,i) = T(j+1,i-1) - T(j,i-1);
         end
-        tmpFx = tmpFx + 1/(i-2) * T(size(X,2)+(2-i),i);
     end
-    Fx = (1/h) * tmpFx;
+    if s==1
+        for i = 3:n+1
+            tmpFx = tmpFx + 1/(i-2) * T(n+(2-i),i);
+        end
+        Fx = (1/h) * tmpFx;
+    end
+    if s==2
+        a=[1,1,11/12];
+        for j = 4:n+1
+            tmpFx = tmpFx + (a(j-n+2) * T(n+(2-j),j));
+        end
+        Fx = (1/(h^2)) * tmpFx;
+    end
+    if s==3
+        a=[1,3/2,7/4,45/24];
+        for j = 5:n+1
+            disp(a(j-n+1));
+            disp(T(n+(2-j),j));
+            tmpFx = tmpFx + (a(j-n+1) * T(n+(2-j),j));
+        end
+        Fx = (1/(h^3)) * tmpFx;
+    end
 end
